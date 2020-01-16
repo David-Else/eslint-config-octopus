35df0df3a3c24fd257e32e273d6fa3685dc94ba4
David <12832280+David-Else@users.noreply.github.com>
2020-01-16 20:31:51 +0000

    Refactor view

--- src/view.ts
+++ src/view.ts
@@ -1,19 +1,23 @@
 export function writeStatsToConsole(removedRules: { [key: string]: string[] }) {
   const bold = (text: string) => `\x1b[1m${text}\x1b[0m`;
 
-  console.log(`${`${Object.values(removedRules)
+  const totalNumberOfRules = Object.values(removedRules)
     .map(ruleNames => ruleNames.length)
-    .reduce((total, noOfRules) => total + noOfRules)} rules were removed`}
-----------------------
-${Object.entries(removedRules)
-  .map(([ruleDescription, ruleNames]) => [
-    `${ruleNames.length} were ${bold(ruleDescription)}
+    .reduce((total, noOfRules) => total + noOfRules);
 
-    ${ruleNames.map(ruleName => ruleName).join('\n')}
+  const removedRulesList = Object.entries(removedRules)
+    .map(([ruleDescription, ruleNames]) => [
+      `${ruleNames.length} were ${bold(ruleDescription)}
+      
+${ruleNames.map(ruleName => ruleName).join('\n')}
 
 `
-  ])
-  .join('')}
-  
-  `);
+    ])
+    .join('');
+
+  console.log(`
+${totalNumberOfRules} rules were removed
+----------------------
+${removedRulesList}
+`);
 }

5bb7d523bde7c21884f8cfd3227cb14ce82ec20f
David <12832280+David-Else@users.noreply.github.com>
2020-04-29 15:57:31 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,5 @@
-export { assert, assertEquals } from "testing/asserts.ts";
-export { green, bold } from "fmt/colors.ts";
+export {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@v0.41.0/testing/asserts.ts";
+export { green, bold } from "https://deno.land/std@v0.41.0/fmt/colors.ts";

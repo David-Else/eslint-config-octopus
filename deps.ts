48b2c6a3aad5498afd8cd9d313d1ba91f78c27e6
David <12832280+David-Else@users.noreply.github.com>
2020-09-18 23:22:40 +0100

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // to update use `udd deps.ts --test="deno test --allow-run ./mod_test.ts"`
 
-export { green, bold } from "https://deno.land/std@0.69.0/fmt/colors.ts";
-export { fromFileUrl } from "https://deno.land/std@0.69.0/path/mod.ts";
+export { green, bold } from "https://deno.land/std@0.70.0/fmt/colors.ts";
+export { fromFileUrl } from "https://deno.land/std@0.70.0/path/mod.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.69.0/testing/asserts.ts";
+} from "https://deno.land/std@0.70.0/testing/asserts.ts";
